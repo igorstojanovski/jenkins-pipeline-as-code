bbc9cf7cf77dbf19cba6d45a7c0489d42ecf2e93
Igor Stojanovski <igorce@outlook.com>
2019-11-17 11:03:28 +0100

    Initial commit
    Added a very simple and empty Spring Boot project
    that will be used as a test subject to build a mutlibranch
    pipeline in Jenkins.

---
+++ src/main/java/org/igorski/pipelineascodedemo/PipelineAsCodeDemoApplication.java
@@ -0,0 +1,13 @@
+package org.igorski.pipelineascodedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineAsCodeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineAsCodeDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/igorski/pipelineascodedemo/PipelineAsCodeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.igorski.pipelineascodedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineAsCodeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
